5065ed35f6c572d1d3de9db436d59cbefab400fc
Daanikus <daanikus@gmail.com>
2019-05-03 10:47:55 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## bundlegen
+
+Utility program for CodeLingo. Run in bundle directory to generate the lingo_bundle file. Only edits tenet list. Other properties still have to be edited manually.
+
+### Usage
+
+`go install`
+
+`bundlegen <path/to/bundle/dir>`
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type BundleFile struct {
+	Description string   `yaml:"description"`
+	Version     string   `yaml:"version"`
+	Tenets      []string `yaml:"tenets"`
+	Tags        []string `yaml:"tags"`
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal(errors.New("supply path to lingo_bundle.yaml"))
+	}
+	wd := os.Args[1]
+
+	dirs, err := ioutil.ReadDir(wd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	yamlData, err := ioutil.ReadFile(wd + "/lingo_bundle.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var file BundleFile
+	if err := yaml.Unmarshal(yamlData, &file); err != nil {
+		log.Fatal(err)
+	}
+
+	var tenets []string
+	for _, dir := range dirs {
+		tenets = append(tenets, dir.Name())
+	}
+
+	file.Tenets = tenets
+
+	newData, err := yaml.Marshal(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(wd+"/lingo_bundle.yaml", newData, 0664)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Wrote lingo_bundle.yaml in", wd)
+}
